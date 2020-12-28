73f9631e5c7e655b566b61ddb6a2c840dd737cc0
Nathaniel Young <nyoung125@gmail.com>
2020-12-28 13:26:25 -0500

    add code to actually send the email

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,7 +1,33 @@
-async function sendMail(event, context) {
-  console.log(event);
+import AWS from 'aws-sdk';
 
-  return event;
+const ses = new AWS.SES({ region: 'us-east-1' });
+
+async function sendMail(event, context) {
+  const params = {
+    Source: 'nyoung125@gmail.com',
+    Destination: {
+      ToAddresses: [ 'nyoung125@gmail.com' ],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from SES!',
+        },
+      },
+      Subject: {
+        Data: 'Test Email',
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const handler = sendMail;
