b1ee1db01ac40e01784e8eb3d06dfa9b5a27d7b5
Nathaniel Young <nyoung125@gmail.com>
2020-12-28 15:02:57 -0500

    add sqs mail queue, consume data in handler

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'us-east-1' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'nyoung125@gmail.com',
     Destination: {
-      ToAddresses: [ 'nyoung125@gmail.com' ],
+      ToAddresses: [ recipient ],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from SES!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Email',
+        Data: subject,
       },
     },
   };
