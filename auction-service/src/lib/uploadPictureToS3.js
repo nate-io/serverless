36ac29577ef04fb2ab0575c3fabaa6f97c0300bd
Nathaniel Young <nyoung125@gmail.com>
2020-12-29 10:54:23 -0500

    add real upload functionality

---
+++ auction-service/src/lib/uploadPictureToS3.js
@@ -0,0 +1,20 @@
+import AWS from 'aws-sdk';
+
+const s3 = new AWS.S3();
+
+/**
+ * Send the param data to S3 for storage.
+ * @param {string} key the name of the S3 object
+ * @param {string} body the data of the S3 object
+ */
+export async function uploadPictureToS3(key, body) {
+  const result = await s3.upload({
+    Bucket: process.env.AUCTIONS_BUCKET_NAME,
+    Key: key,
+    Body: body,
+    ContentEncoding: 'base64',
+    ContentType: 'image/jpeg', // works for png as well
+  }).promise();
+
+  return result;
+}
