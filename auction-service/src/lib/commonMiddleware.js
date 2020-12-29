3a0267c6da31f10b4ca7e78c3f1f459a8a8d2111
Nathaniel Young <nyoung125@gmail.com>
2020-12-29 13:02:47 -0500

    add frontend. add comment

--- auction-service/src/lib/commonMiddleware.js
+++ auction-service/src/lib/commonMiddleware.js
@@ -5,6 +5,7 @@ import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 // cleaner error handling
 import httpErrorHandler from '@middy/http-error-handler';
+// set CORS headers in responses
 import cors from '@middy/http-cors';
 
 export default handler => middy(handler)
