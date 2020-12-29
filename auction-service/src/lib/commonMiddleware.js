de70f814df9cc23d8fcf40978ec75786e2b63508
Nathaniel Young <nyoung125@gmail.com>
2020-12-29 12:13:38 -0500

    enable cors for front end app

--- auction-service/src/lib/commonMiddleware.js
+++ auction-service/src/lib/commonMiddleware.js
@@ -5,10 +5,12 @@ import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 // cleaner error handling
 import httpErrorHandler from '@middy/http-error-handler';
+import cors from '@middy/http-cors';
 
 export default handler => middy(handler)
   .use([
     httpJsonBodyParser(),
     httpEventNormalizer(),
     httpErrorHandler(),
+    cors(),
   ]);
